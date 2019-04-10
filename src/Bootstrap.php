73fee19a835fd7b888087dc7f803770e8b66005a
jarrodized <jarrodized@yahoo.com>
2019-04-10 19:04:12 -0400

    Copy from no-framework repo

---
+++ src/Bootstrap.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+define('ROOT_DIR', dirname(__DIR__));
+
+require ROOT_DIR . '/vendor/autoload.php';
+
+\Tracy\Debugger::enable();
+
+$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+
+$controller = new \SocialNews\FrontPage\Presentation\FrontPageController();
+$response = $controller->show($request);
+
+if (!$response instanceof \Symfony\Component\HttpFoundation\Response)
+{
+    throw new \Exception('Controller methods must return a Response object');
+}
+
+$response->prepare($request);
+$response->send();
